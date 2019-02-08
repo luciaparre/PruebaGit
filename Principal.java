d65551fa5f5770b832ca03bf4a7c382f7a4785e5
luciaparre <46563525+luciaparre@users.noreply.github.com>
2019-02-08 11:18:36 +0100

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,12 @@
+package notasAlumno;
+
+public class Principal { 
+	
+	public static void main(String[] args) {
+		Alumno al = new Alumno();
+		System.out.println(al.getNota1()); 
+		System.out.println(al.getNota2());
+		System.out.println(al.getNota3());
+	}
+
+}
